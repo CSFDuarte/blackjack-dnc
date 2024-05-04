b30cbe415cdf929447810f96a9c314ea24d05795
Caique Duarte <caique.duarte@sharkdev.com.br>
2024-05-04 08:55:09 -0300

    feat: Voltando ao inicio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
-import Dealer, { CARD_BACKGROUND } from '@/services/dealer';
+import Dealer, { CARD_BACKGROUND, ICard, IDeck } from '@/services/dealer';
 import { translateSuit, translateValue } from '@/services/translation';
-import { Button, Container, Grid, Input, Typography } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import React, { useState } from 'react';
 
 const dealer = new Dealer();
@@ -13,20 +13,12 @@ function App() {
     shuffled: false,
   });
   const [history, setHistory] = useState([]);
-  const [score, setScore] = useState({
-    score: '0',
-    result: null,
-    game: false,
-  });
-
-  //? Sentindo falta de algo? Talvez alguma informação nova que precise ser atualizada na tela?
 
   const shuffleDeck = async () => {
     try {
       setDeck(await dealer.shuffle());
       setCard(null);
       setHistory([]);
-      setScore({ score: '0', result: null, game: true, money: 0});
     } catch (error) {
       console.error('Falha ao embaralhar:', error);
     }
@@ -36,82 +28,51 @@ function App() {
     try {
       setDeck({ ...deck, remaining: deck.remaining - 1, shuffled: deck.remaining > 1})
       setCard(await dealer.draw());
-      setHistory(dealer.getHistory());
-      const score = dealer.getScore(bet)
-      setScore(score);
+      setHistory(await dealer.getHistory());
     } catch (error) {
       console.error('Falha ao comprar carta:', error);
     }
   };
 
-  const stopGame = () => {
-    //? O que será que tem que ser feito aqui?
-  };
-
   return (
     <Container>
       <Grid container justifyContent="center" alignItems="flex-start" direction={'row'} my={1}>
-        <Grid justifyContent="center" alignItems="flex-start" my={2} sm={12} md={6}>
+
+        {/* BARALHO DE CARTAS */}
+        <Grid item justifyContent="center" alignItems="flex-start" my={2} sm={12} md={6}>
           <Grid container direction={"column"}>
-            {/* CABEÇALHO */}
             <Typography variant="h4" align="center">
-              Simulador de Blackjack
+              Simulador de baralhos
             </Typography>
             <Typography variant="h5" align="center">
               DNC Treinamentos
             </Typography>
-
-            {/* APOSTA E CARTEIRA AQUI BB ;D */}            
-            
-            {!score.game && (
-              <Button variant="contained" onClick={shuffleDeck} style={{ margin: 'auto' }}>
-                Iniciar jogo
-              </Button>
-            )}
+            <Button variant="contained" onClick={shuffleDeck} style={{ margin: 'auto' }}>
+              {deck.deck_id === '' ? 'Novo baralho' : deck.shuffled ? 'Embaralhar novamente' : 'Embaralhar'}
+            </Button>
           </Grid>
-
+          
           {deck.shuffled && (
             <Container style={{ textAlign: 'center' }}>
-              {/* BARALHO DE CARTAS */}
-              <img src={CARD_BACKGROUND} style={{ maxWidth: '100%', borderRadius: '5px' }} />
-
-              {/* PLACAR ATUAL */}
-              {score.score && (<>
-                <Typography variant="h6" align="center">
-                  Pontuação: {score.score}
-                </Typography>
-                <Typography variant="h6" align="center">
-                  Cartas compradas: {52 - deck.remaining}
-                </Typography>
-              </>)}
-
-              {/* RESULTADO FINAL */}
-              {score.result && (
-                <Typography variant="h6" align="center">
-                  Resultado: {score.result}
-                </Typography>
-              )}
-
-              {/* AÇÒES DO JOGO */}
-              {score.game &&(<>
-                <Grid container justifyContent="center" alignItems="center" direction={'column'} my={1}>
-                  <Button variant="contained" onClick={drawCard} style={{ margin: 'auto', width: '180px', backgroundColor: 'green' }}>
-                    Comprar carta
-                  </Button>
-                  {/* SERÁ QUE VOU COMPRAR PARA SEMPRE? */}
-                </Grid>
-              </>)}
+              <img src={CARD_BACKGROUND} style={{ maxWidth: '100%', borderRadius: '5px'}} />
+              <Typography variant="h6" align="center" hidden={deck.deck_id === ''}>
+                ID: {deck.deck_id} - Cartas restantes: {deck.remaining}
+              </Typography>
+              <Button variant="contained" onClick={drawCard} disabled={!deck.shuffled} style={{ margin: 'auto'}}>
+                Comprar carta
+              </Button>
             </Container>
           )}
         </Grid>
-        <Grid item justifyContent="center" alignItems="center" my={2} sm={12} md={6}>
+
+        <Grid item justifyContent="center" alignItems="center" direction={'column'} my={2} sm={12} md={6}>
           {/* CARTA COMPRADA */}
           {card && (<>
             <Typography variant="h4" align="center">
               Carta comprada
             </Typography>
             <Container style={{ textAlign: 'center', marginTop: '20px' }}>
-              <img src={card.image} alt={card.code} style={{ maxWidth: '100px', borderRadius: '5px' }} />
+              <img src={card.image} alt={card.code} style={{ maxWidth: '100%', borderRadius: '5px' }} />
               <Typography variant="h6" align="center">
                 {translateValue(card.value) + " de " + translateSuit(card.suit)}
               </Typography>
@@ -120,8 +81,8 @@ function App() {
 
           {/* HISTÓRICO DE CARTAS COMPRADAS */}
           {history.length > 0 && (<>
-            <Typography variant="h4" align="center" my={1}>
-              Sua mão
+            <Typography variant="h4" align="center">
+              Histórico de cartas
             </Typography>
             <Grid container justifyContent="center" alignItems="center" direction={'row'} my={2}>
               {history.map((card, index) => (
--- src/services/dealer.js
+++ src/services/dealer.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://deckofcardsapi.com/api',
+  baseURL: 'https://deckofcardsapi.com/api/',
 });
 
 export const CARD_BACKGROUND = 'https://deckofcardsapi.com/static/img/back.png';
@@ -13,93 +13,38 @@ export default class Dealer {
     shuffled: false,
   };
 
-  score = [0];
   history = [];
 
-  //* Embaralha o baralho
   async shuffle() {
-    const response = 
-      this.deck.deck_id === '' ?
-        await api.get('/deck/new/shuffle/?deck_count=1') :
-        await api.get(`/deck/${this.deck.deck_id}/shuffle`);
+    try {
+      const response = 
+        this.deck.deck_id === '' ?
+          await api.get('deck/new/shuffle/?deck_count=1') :
+          await api.get(`deck/${this.deck.deck_id}/shuffle`);
 
-    this.deck.deck_id = response.data.deck_id;
-    this.deck.remaining = response.data.remaining;
-    this.deck.shuffled = true;
-    this.history = [];
+      this.deck.deck_id = response.data.deck_id;
+      this.deck.remaining = response.data.remaining;
+      this.deck.shuffled = true;
 
-    return this.deck;
-  }
-
-  //* Compra uma carta
-  async draw() {
-    const response = await api.get(`/deck/${this.deck.deck_id}/draw/?count=1`);
-    this.history.push(response.data.cards[0]);
-    return response.data.cards[0];
-  }
-
-  //* Retorna o histórico de cartas
-  getHistory() {
-    return this.history;
-  }
-
-  //* Atualiza o placar do jogo
-  updateScore() {
-    this.score = [0];
-    for (let i = 0; i < this.history.length; i++) {
-      const card = this.history[i];
-      //* Para cada carta, ele vai somar o valor dela em todos os scores
-      this.score.forEach((value, index) => {
-        this.score[index] +=
-          card.value === 'ACE' ? 1 :
-          card.value === 'JACK' || card.value === 'QUEEN' || card.value === 'KING' ? 10 :
-          parseInt(card.value);
-      });
-
-      
-      //* Se a carta for um Ás ele adiciona um novo resultado com o valor 1
-      if(card.value === 'ACE' && this.score.length == 1)
-          this.score.push(this.score[0] + 10);
+      return this.deck;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Falha ao embaralhar o baralho.');
     }
-
-    //* Remove os valores maiores que 21
-    this.score = this.score.filter(value => value <= 21);
-    return this.score;
   }
 
-  //* Calcula o resultado da aposta
-  getBetResult(result, bet) { //? Função já feita, mole, mas onde usar?
-    if (result === 'Blackjack!')
-      return bet * 2.5;
-    if (result === 'Perdeu!' || (this.score[1] || this.score[0]) < 15) 
-      return 0;
-    if (result === null) 
-      return bet * (1 + (((this.score[1] || this.score[0]) - 10)/100));
+  async draw() {
+    try{
+      const response = await api.get(`deck/${this.deck.deck_id}/draw/?count=1`);
+      this.history.push(response.data.cards[0]);
+      return response.data.cards[0];
+    } catch (error) {
+      console.error(error);
+      throw new Error('Falha ao comprar carta.'); 
+    }
   }
 
-  //* Retorna o placar do jogo
-  getScore(bet) { //? O que será este bet?
-    //* Atualiza o placar em numbers
-    const score = this.updateScore();
-
-    //* Transforma o placar em string
-    let answer = '';
-    score.forEach((value, index) => 
-      answer += value + (index < score.length - 1 ? ' ou ' : ''));
-
-    //* Verifica o resultado do jogo
-    const result =
-      score.length === 0 ?
-        'Perdeu!'
-      : score.length > 0 && score.includes(21) ?
-        'Blackjack!'
-      :
-        null;
-
-    return {
-      score: answer,
-      result: result,
-      game: !result,
-    };
+  async getHistory() {
+    return this.history;
   }
 }
